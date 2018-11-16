213a38b0cedc5dcca57b137923e9601957b86f17
Femi Coker <fcoker@outlook.com>
2018-11-16 15:46:04 +0000

    comment controller added

---
+++ controllers/commentController.js
@@ -0,0 +1,30 @@
+const Event = require('../models/event');
+
+function createRoute(req, res, next) {
+  Event.findById(req.params.eventId)
+    .then(event => {
+      req.body.user = req.tokenUserId;
+      event.comments.push(req.body);
+      return event.save();
+    })
+    .then(event => Event.populate(event, 'createdBy comments.user'))
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function deleteRoute(req, res, next) {
+  Event.findById(req.params.eventId)
+    .then(event => {
+      const comment = event.comments.id(req.params.commentId);
+      comment.remove();
+      return event.save();
+    })
+    .then(event => Event.populate(event, 'createdBy comments.user'))
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+module.exports = {
+  create: createRoute,
+  delete: deleteRoute
+};
