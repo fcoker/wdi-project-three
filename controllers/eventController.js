5e503b5a353960ba05675bff50d6c456240ee3e5
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-11-16 15:37:55 +0000

    added eventController

---
+++ controllers/eventController.js
@@ -0,0 +1,41 @@
+const Event = require('../models/event');
+
+function indexRoute(req, res, next) {
+  Event.find().then(events => res.json(events))
+    .catch(next);
+}
+
+function showRoute(req, res, next) {
+  Event.findById(req.params.eventId)
+    .populate('createdBy comments.user')
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function createRoute(req, res, next) {
+  Event.create(req.body)
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function updateRoute(req, res, next) {
+  Event.findById(req.params.eventId)
+    .then(event => event.set(req.body))
+    .then(event => event.save())
+    .then(event => res.json(event))
+    .catch(next);
+}
+
+function deleteRoute(req, res, next) {
+  Event.findByIdAndDelete(req.params.eventId)
+    .then(() => res.sendStatus(204))
+    .catch(next);
+}
+
+module.exports = {
+  index: indexRoute,
+  show: showRoute,
+  create: createRoute,
+  update: updateRoute,
+  delete: deleteRoute
+};
