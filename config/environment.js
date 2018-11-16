c09e2f9ec4f2689184eba7511583f860cdd4f747
ShamSZ <sham.szoda@gmail.com>
2018-11-16 13:24:15 +0000

    Set up directory architecture and environment.

--- config/environment.js
+++ config/environment.js
@@ -1,3 +1,9 @@
 const port = process.env.PORT || 4000;
+const dbUri = process.env.MONGODB_URI || 'mongodb://localhost/events';
+const secret = process.env.SECRET || 'black horse';
 
-module.exports = { port };
+module.exports = {
+  port: port,
+  dbUri: dbUri,
+  secret: secret
+};
