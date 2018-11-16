f3b24920c87204ea5b394efce69d45932c30d1de
Femi Coker <fcoker@outlook.com>
2018-11-16 13:47:13 +0000

    console log sham

--- index.js
+++ index.js
@@ -5,6 +5,12 @@ const { port } = require('./config/environment');
 
 app.use(express.static(`${__dirname}/public`));
 
+//done by femi to test features
+function greeting(){
+  console.log('This is Group Shagrami');
+}
+
+greeting();
 app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
 
 app.listen(port, () => console.log(`Express is running on port ${port}`));
