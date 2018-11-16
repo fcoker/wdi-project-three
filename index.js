3c8efd97a6cfa5520d35077c99737c33d4c1e412
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-11-16 14:14:41 +0000

    setup index.js

--- index.js
+++ index.js
@@ -1,27 +1,29 @@
 const express = require('express');
 const app = express();
+const env = require('./config/environment');
+const mongoose = require('mongoose');
+mongoose.connect(env.dbUri);
+const router = require('./config/router');
 
-//sham's code
-console.log('testing for merge conflicts');
+const bodyParser = require('body-parser');
+const morgan = require('morgan');
 
-const { port } = require('./config/environment');
-// Grant starting here
-const test = function() {
-  return console.log('Hello');
-};
+app.use(bodyParser.json());
+app.use(morgan('dev'));
+
+app.use('/api', router);
+
+app.use(function(error, req, res, next) {
+  console.log('There was an error', error);
+  if (error.name === 'ValidationError') {
+    res.status(422).json(error.errors);
+  } else if (error) {
+    res.status(500).send(error._message);
+  } else {
+    next();
+  }
+});
 
 
 
-app.use(express.static(`${__dirname}/public`));
-
-//done by femi to test features
-function greeting(){
-  console.log('This is Group Shagrami');
-}
-
-greeting();
-app.get('/*', (req, res) => res.sendFile(`${__dirname}/public/index.html`));
-
-app.listen(port, () => console.log(`Express is running on port ${port}`));
-
-module.exports = app;
+app.listen(env.port, () => console.log(`Express is running on port ${env.port}`));
