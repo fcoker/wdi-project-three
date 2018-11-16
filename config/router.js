41ed0d4bcc43d299771104b231df45e7b6f5e9c4
Grant Wilkinson <wilkinsonmgrant@gmail.com>
2018-11-16 14:48:08 +0000

    Added router and secure route

---
+++ config/router.js
@@ -0,0 +1,47 @@
+const env = require('./environment');
+const router = require('express').Router();
+const eventController = require('../controllers/eventController');
+const authController = require('../controllers/authController');
+const userController = require('../controllers/userController');
+const commentController = require('../controllers/commentController');
+const jwt = require('jsonwebtoken');
+
+function secureRoute(req, res, next) {
+  if (!req.headers.authorization)
+    res.status(401).json({ message: 'Unauthorised'});
+  const token = req.headers.authorization.replace('Bearer ', '');
+  jwt.verify(token, env.secret, function(err) {
+    if (err) {
+      res.status(401).json({ message: 'Unauthorised!' });
+    } else {
+      req.tokenUserId = jwt.decode(token).sub;
+      next();
+    }
+  });
+}
+
+router.route('/register')
+  .post(authController.register);
+
+router.route('/login')
+  .post(authController.login);
+
+router.route('/events')
+  .get(eventController.index)
+  .post(secureRoute, eventController.create);
+
+router.route('/events/:eventId')
+  .get(eventController.show)
+  .put(secureRoute, eventController.update)
+  .delete(secureRoute, eventController.delete);
+
+router.route('/events/:eventId/comments')
+  .post(secureRoute, commentController.create);
+
+router.route('/events/:eventId/comments/:commentId')
+  .delete(secureRoute, commentController.delete);
+
+router.route('/users/:userId')
+  .get(userController.show);
+
+module.exports = router;
