8fdfe7dd21462d490cc6c2c8b0d064a9ec555aa7
ShamSZ <sham.szoda@gmail.com>
2018-11-19 09:28:24 +0000

    Created frontend events show ctrl

---
+++ src/controllers/events/showCtrl.js
@@ -0,0 +1,36 @@
+function showCtrl($state, $scope, $http) {
+  $scope.comment = {};
+  $http({
+    method: 'GET',
+    url: `/api/events/${$state.params.eventId}`
+  }).then(result => {
+    $scope.event = result.data;
+  });
+
+  $scope.createComment = function() {
+    $http({
+      method: 'POST',
+      url: `/api/events/${$state.params.eventId}/comments`,
+      data: $scope.comment
+    }).then(result => {
+      $scope.event = result.data;
+      $scope.comment.text = null;
+    });
+  };
+
+  $scope.deleteComment = function(comment) {
+    $http({
+      method: 'DELETE',
+      url: `/api/events/${$state.params.eventId}/comments/${comment._id}`
+    }).then(result => $scope.event = result.data);
+  };
+
+  $scope.handleDeleteEvent = function() {
+    $http({
+      method: 'DELETE',
+      url: `/api/events/${$scope.event._id}`
+    }).then(() => $state.go('eventsIndex'));
+  };
+}
+
+export default showCtrl;
