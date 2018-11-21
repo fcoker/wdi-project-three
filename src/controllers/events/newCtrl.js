b1b4bf86a58cad774d362c7111f7780305c8b57a
Femi Coker <fcoker@outlook.com>
2018-11-21 15:51:52 +0000

    drop down search bar added for create new view

--- src/controllers/events/newCtrl.js
+++ src/controllers/events/newCtrl.js
@@ -1,3 +1,5 @@
+import mapLib from '../../lib/map';
+
 function newCtrl($scope, $http, $state, $auth) {
   $scope.createEvent = function() {
     $scope.event.createdBy = $auth.getPayload().sub;
@@ -11,7 +13,18 @@ function newCtrl($scope, $http, $state, $auth) {
       console.log(error);
       $scope.errors = error.data;
     });
+  };  
+  $scope.findPlaces = function() {
+    // Make a request to nominatim, using the current search term:
+    $http({
+      method: 'GET',
+      url: `https://nominatim.openstreetmap.org/search/${$scope.searchTerm}?format=json&limit=5`
+    }).then(result => {
+      // result.data now has the top 5 results.
+      $scope.searchResults = result.data;
+    });
   };
 }
+
 
 export default newCtrl;
--- src/controllers/events/showCtrl.js
+++ src/controllers/events/showCtrl.js
@@ -75,32 +75,7 @@ function showCtrl($state, $scope, $http) {
       });
     }
   };
-  // Functions
-  $scope.panMap = function(country) {
-    mapLib.panTo(country.latlng);
-    mapLib.clearMarkers();
-    mapLib.addMarker(country.latlng, `<strong>${country.name}</strong><img src=${country.flag} />`);
-  };
 
-  $scope.findPlaces = function() {
-    // Make a request to nominatim, using the current search term:
-    $http({
-      method: 'GET',
-      url: `https://nominatim.openstreetmap.org/search/${$scope.searchTerm}?format=json&limit=5`
-    }).then(result => {
-      // result.data now has the top 5 results.
-      $scope.searchResults = result.data;
-    });
-  };
-
-  $scope.goTo = function(place) {
-    console.log('Clicked on', place);
-    // Clear the search text:
-    $scope.searchTerm = null;
-    mapLib.panTo([place.lat, place.lon]);
-    mapLib.addMarker([place.lat, place.lon], place.display_name);
-    $scope.searchResults = null;
-  };
 
   $scope.findUser = function() {
     navigator.geolocation.getCurrentPosition(function(result) {
