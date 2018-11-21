ecbf3c59ff9e4bbe5a114a00b8a63de6763e1c5c
Femi Coker <fcoker@outlook.com>
2018-11-21 21:33:31 +0000

    event description in show added, minimal styling to nav-bar

--- src/controllers/events/newCtrl.js
+++ src/controllers/events/newCtrl.js
@@ -1,5 +1,3 @@
-import mapLib from '../../lib/map';
-
 function newCtrl($scope, $http, $state, $auth) {
   $scope.createEvent = function() {
     $scope.event.createdBy = $auth.getPayload().sub;
@@ -13,14 +11,12 @@ function newCtrl($scope, $http, $state, $auth) {
       console.log(error);
       $scope.errors = error.data;
     });
-  };  
+  };
   $scope.findPlaces = function() {
-    // Make a request to nominatim, using the current search term:
     $http({
       method: 'GET',
       url: `https://nominatim.openstreetmap.org/search/${$scope.searchTerm}?format=json&limit=5`
     }).then(result => {
-      // result.data now has the top 5 results.
       $scope.searchResults = result.data;
     });
   };
