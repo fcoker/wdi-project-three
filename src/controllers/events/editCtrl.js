af3e6e25e59f293f0d3e60b9000eace223acd611
ShamSZ <sham.szoda@gmail.com>
2018-11-22 18:10:19 +0000

    Styled new and edit forms, added location autofill to edit page

--- src/controllers/events/editCtrl.js
+++ src/controllers/events/editCtrl.js
@@ -4,11 +4,26 @@ function editCtrl($scope, $state, $http) {
     url: `/api/events/${$state.params.eventId}`
   }).then(result => $scope.event = result.data);
   $scope.editEvent = function() {
+    $scope.event.location = $scope.searchTerm;
     $http({
       method: 'PUT',
       url: `/api/events/${$state.params.eventId}`,
       data: $scope.event
     }).then(() => $state.go('eventShow', { eventId: $state.params.eventId }));
+  };
+  $scope.findPlaces = function() {
+    $http({
+      method: 'GET',
+      url: `https://nominatim.openstreetmap.org/search/${$scope.searchTerm}?format=json&limit=5`
+    }).then(result => {
+      $scope.searchResults = result.data;
+    });
+  };
+  $scope.handleSelection = function(place) {
+    console.log(place);
+    $scope.searchTerm = place.display_name;
+
+    $scope.searchResults = null;
   };
 }
 
