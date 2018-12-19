e634f63085163557008190a0e082c2f7393ccda0
ShamSZ <sham.szoda@gmail.com>
2018-12-19 15:11:47 +0000

    Rearranged show page edit/delete buttons. Sorted users eventsAttending list by date. Moved locate user on map button to be visible and clickable for mobile devices.

--- src/controllers/users/showCtrl.js
+++ src/controllers/users/showCtrl.js
@@ -3,6 +3,9 @@ function showCtrl($state, $scope, $http) {
     method: 'GET',
     url: `/api/users/${$state.params.userId}`
   }).then(result => {
+    result.data.eventsAttending.sort(function(event1, event2){
+      return new Date(event1.date) - new Date(event2.date);
+    });
     $scope.user = result.data;
   });
 }
